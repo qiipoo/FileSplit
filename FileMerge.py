9588706f73c48c5a2e0b0238437629d08288addc
qiipoo <peigs@outlook.com>
2023-01-08 19:59:10 +0800

    modify

--- FileMerge.py
+++ FileMerge.py
@@ -2,8 +2,6 @@
 from PyPDF2 import PdfReader
 from PyPDF2 import PdfWriter
 from PyPDF2 import PdfMerger
-import sys
-import copy
 import os
 
 file_path = r'C:\Users\JT-0919\Desktop\Hi\YQP'
@@ -12,8 +10,8 @@ file_path = r'C:\Users\JT-0919\Desktop\Hi\YQP'
 def merge_file():
     in_files = [
         'scan_20221222120359.pdf',
-        'NEW2.pdf']
-    out_file = 'NEW.pdf'
+        'NEW_DEL.pdf']
+    out_file = 'NEW_OUT.pdf'
     merger = PdfMerger()
     for pdf in in_files:
         reader = PdfReader(os.path.join(file_path, pdf))
@@ -25,20 +23,33 @@ def merge_file():
 
 
 def del_file(del_num):
-    in_file = r'技术服务方案.pdf'
-    out_file = r'NEW2.pdf'
+    in_file = r'C359 省委军民融合办关于征求《湖北省支持绿色智能船舶产业发展试点示范若干措施（征求意见稿）》意见的函.pdf'
+    out_file = r'NEW_DEL.pdf'
+
+    if type(del_num) == int:
+        del_multi = False
+    elif type(del_num) == list or type(del_num) == set or type(del_num) == tuple:
+        del_multi = True
+    else:
+        print("Unsupported type!")
+        return
+
     reader = PdfReader(os.path.join(file_path, in_file))
     writer = PdfWriter()
     for num in range(0, reader.getNumPages()):
-        if num == del_num - 1:
-            continue
+        if del_multi:
+            if num + 1 in del_num:
+                continue
+        else:
+            if num + 1 == del_num:
+                continue
         writer.addPage(reader.getPage(num))
     writer.write(os.path.join(file_path, out_file))
 
 
 if __name__ == '__main__':
     try:
-        del_file(2)
+        del_file([1, 2])
         # merge_file()
     except Exception as e:
         print(e)
--- FileSplit.py
+++ FileSplit.py
@@ -1,1 +1,24 @@
 
+import itertools
+
+if __name__ == '__main__':
+
+    print("*" * 80)
+    for each in itertools.product("abc", "xy"):
+        print(each)
+
+    print("*" * 80)
+    for each in itertools.product("abc", repeat=2):
+        print(each)
+
+    print("*" * 80)
+    for each in itertools.permutations("abc", 2):
+        print(each)
+
+    print("*" * 80)
+    for each in itertools.combinations("abc", 2):
+        print(each)
+
+    print("*" * 80)
+    for each in itertools.combinations_with_replacement("abc", 2):
+        print(each)
